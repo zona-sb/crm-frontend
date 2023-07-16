69e2d97657bc46b38cfd35632ce6b73d41042a9f
diewerkself <redead29@gmail.com>
2023-07-16 13:42:52 +0300

    fix Lint

--- src/utils/shared/components/Button/ButtonCustom.jsx
+++ src/utils/shared/components/Button/ButtonCustom.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import cn from 'classnames';
 import './ButtonCustom.css';
 
-const ButtonCustom = ({ children, type, className, color = 'confrim' }) => {
+const ButtonCustom = ({
+  children,
+  className,
+  type = 'button',
+  color = 'confrim',
+}) => {
   const buttonStyle = cn(className, {
     custom__button: true,
     'custom__button-confrim': color === 'confrim',
--- src/utils/shared/components/Modal/ModalCustom.jsx
+++ src/utils/shared/components/Modal/ModalCustom.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import './ModalCustom.css';
 
-const ModalCustom = (props) => {
-  return (
-    <Modal
-      {...props}
-      aria-labelledby='contained-modal-title-vcenter'
-      centered
-      className='custom__modal'
-    >
-      {props.children}
-    </Modal>
-  );
-};
+const ModalCustom = (props) => (
+  <Modal
+    {...props}
+    aria-labelledby='contained-modal-title-vcenter'
+    centered
+    className='custom__modal'
+  >
+    {props.children}
+  </Modal>
+);
 
 export default ModalCustom;

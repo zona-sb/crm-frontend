c15c85b892ff2e3ec27c9f737dfc29bd777fdf74
diewerkself <redead29@gmail.com>
2023-12-01 16:29:15 +0300

    refactor: фильтрация

--- src/utils/filtering.js
+++ src/utils/filtering.js
@@ -3,7 +3,7 @@ const filtering = (data, filterValue, setFilterValue) => {
   if (newFilter.value.trim().length === 0) {
     newFilter.value = null;
   }
-  setFilterValue({ ...filterValue, [newFilter.key]: newFilter.value, page: 1 });
+  setFilterValue({ ...filterValue, [newFilter.key]: newFilter.value });
 };
 
 export default filtering;
